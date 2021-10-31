b4f452b935d03f54a594d8dbb42873daff2be19c
Muhammad Gilang Ramadhan <80266785+gilanglahat22@users.noreply.github.com>
2021-10-31 22:53:52 +0700

    Update ability.h

--- ADT/move/ability.h
+++ ADT/move/ability.h
@@ -16,18 +16,4 @@ boolean IsIncreaseCapacity_Actived(BAG B, char Dest);
 boolean IsReturnToSender_Actived(char COMMAND);
 /* Mengirimkan true jika ability Return To sender Aktif */
 
-/* void SpeedBoost(TIME *time); */
-/* Menambahkan efek Speed Boost pada time apabila Mobita Berhasil Mengantarkan barang berat (Heavy Item) */
-/* Efeknya : Waktu akan bertambah 1 unit setiap Mobita berpindah 2 lokasi. Efek ini berlangsung hingga Mobita telah melewati 10 lokasi ( berlaku selama 5 unit waktu) */
-/* Ability ini akan segera hilang bila terdapat Heavy Item pada Bag Mobita */
-
-/* void IncreaseCapacity(BAG bag, int *CAPACITY); */
-/* Menambah kapasitas tas sebanyak 1 satuan */
-
-/* BONUS */
-/* void ReturnToSender(TIME *time, TO_DO_LIST list); */
-/* Jika ability ini digunakan dengan command "RETURN", maka item di atas tas dikembalikan ke lokasi pick up. */
-/* Efeknya : 1. Pesanan akan dikembalikan ke TO DO List di urutan terakhir, 2. Time Limit yang terdapat pada pesanan dengan perishable item akan direset sebelum dikembalikan */
-/* Ability ini tidak dapat diaktifkan untuk pesanan VIP Item */
-
 #endif
