045cbdfe9664115e079e7efd442e13a80aa1dda4
Muhammad Gilang Ramadhan <80266785+gilanglahat22@users.noreply.github.com>
2021-11-09 12:36:10 +0700

    Update ability.c

--- ADT/move/ability.c
+++ ADT/move/ability.c
@@ -2,18 +2,77 @@
 #include "move.h"
 #include "ability.h"
 
-boolean IsSpeedBoost_Actived(BAG B, char Dest)
+boolean IsSpeedBoost_Actived(BAG B, char Dest, int count_actived)
 /* Mengirimkan true jika speed boost ability aktif */
 {
-    return (isTheItemDone(B, 'H', Dest) == true);
+    if (count_actived < 10 && (isTheItemDone(B, 'H', Dest) == true))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 boolean IsIncreaseCapacity_Actived(BAG B, char Dest)
 /* Mengirimkan true jika ability Increase Capacity aktif */
 {
-    return (isTheItemDone(B, 'P', Dest) == true);
+    if (isTheItemDone(B, 'P', Dest) == true)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
-boolean IsReturnToSender_Actived(char COMMAND)
+boolean IsReturnToSender_Actived(char COMMAND[6])
 /* Mengirimkan true jika ability Return To sender Aktif */
 {
-    return (COMMAND == 'RETURN');
+    for (int i = 0; i < 6; i++)
+    {
+        if (i == 0)
+        {
+            if (COMMAND[i] != 'R')
+            {
+                return false;
+            }
+        }
+        else if (i == 1)
+        {
+            if (COMMAND[i] != 'E')
+            {
+                return false;
+            }
+        }
+        else if (i == 2)
+        {
+            if (COMMAND[i] != 'T')
+            {
+                return false;
+            }
+        }
+        else if (i == 3)
+        {
+            if (COMMAND[i] != 'U')
+            {
+                return false;
+            }
+        }
+        else if (i == 4)
+        {
+            if (COMMAND[i] != 'R')
+            {
+                return false;
+            }
+        }
+        else if (i == 5)
+        {
+            if (COMMAND[i] != 'N')
+            {
+                return false;
+            }
+        }
+    }
+    return true;
 }
