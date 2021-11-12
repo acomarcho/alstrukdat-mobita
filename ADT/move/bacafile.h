64f468fbb507b5cc0d7f2ef7e7ac3b2d1b9d166a
Muhammad Gilang Ramadhan <80266785+gilanglahat22@users.noreply.github.com>
2021-11-12 15:26:14 +0700

    Add files via upload

---
+++ ADT/move/bacafile.h
@@ -0,0 +1,11 @@
+/* File : bacafile.h */
+/* Definisi ADT bacafile */
+
+#include "boolean.h"
+#define MAX 1000
+
+void bacaLine(int Line);
+/* Buat baca isi line */
+
+void bacaFile();
+/* Buat Baca file konfigurasi */
