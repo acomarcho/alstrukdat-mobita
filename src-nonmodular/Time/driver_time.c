509de18dde58f28da5c7c335d894f560a5c34a57
Muhammad Gilang Ramadhan <80266785+gilanglahat22@users.noreply.github.com>
2021-11-17 13:34:46 +0700

    Add files via upload

---
+++ src-nonmodular/Time/driver_time.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "time.h"
+
+// Driver Time, membuat time t kemudian menambahkannya sebesar k yang kemudian hasilnya akan ditampilkan ke layar
+
+int main()
+{
+    TIME t;
+    CreateTime(&t);
+    int k;
+    scanf("%d", &k);
+    increaseTime(&t, k);
+    printf("%d", t);
+    return 0;
+}
+// gcc -o driver_time driver_time.c time
